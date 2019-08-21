22cdc61062440df8489ac151ca14c433a3d7a9c7
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-08-21 16:20:44 -0400

    Initial commit, WIP on scripting and store, basic HTML for calc form

---
+++ scripts/calculators.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const calculators = (function(){
+  let render = `
+    <div class="js-calculator-form"><p>form</p></div>
+    <div class="js-customer-charges-form"><p>charges</p></div>
+    <div class="js-earnings-form"><p>earnings</p></div>
+  `;
+  return {
+    render
+  };
+}());
---
+++ scripts/store.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+const store = (function() {
+  let meals = [
+    {price: 9.99, tax: 7, tip: 20}
+  ];
+  return {
+    meals
+  };
+}());
